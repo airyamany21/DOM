1837a31c4dccace43ff5a3f456ac00478df594f4
airyamany21 <airyamany23@hotmail.com>
2023-02-01 17:54:23 -0600

    clase05 completada

--- components/deleteIcon.js
+++ components/deleteIcon.js
@@ -1,17 +1,23 @@
 
-const deleteIcon = () => {
-    const content = `
-    <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    const i = document.createElement('i');
-    i.classList.add('fas', 'fa-trash-alt', 'trashIcon', 'icon');
-    i.addEventListener('click', deleteTask);
-    return i;
-}
-const deleteTask = (event) => {
-    // console.log('Eliminar tarea');
-    // console.log(event.target.parentElement);
-    const parent = event.target.parentElement;
-    parent.remove();
-}
+import { displayTasks } from "./readTasks.js";
+
+const deleteIcon = (id) => {
+  const i = document.createElement('i');
+  i.classList.add('fas', 'fa-trash-alt', 'trashIcon', 'icon');
+  i.addEventListener('click', () => deleteTask());
+  return i;
+};
+
+const deleteTask = (id) => {
+  const li = document.querySelector("[data-list");
+  const tasks = JSON.parse(localStorage.getItem('tasks'));
+  const index = tasks.findIndex((item) => item.id === id);
+  console.log(index);
+  tasks.splice(index, 1);
+  li.innerHTML = "";
+  console.log(tasks);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  displayTasks();
+};
 
 export default deleteIcon;
